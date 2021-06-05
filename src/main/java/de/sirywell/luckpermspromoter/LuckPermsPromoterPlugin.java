e01205d75a725d5d81221ed91ebb478b9b229eca
SirYwell <hannesgreule@outlook.de>
2021-06-05 11:15:24 +0200

    Make data reloadable

--- src/main/java/de/sirywell/luckpermspromoter/LuckPermsPromoterPlugin.java
+++ src/main/java/de/sirywell/luckpermspromoter/LuckPermsPromoterPlugin.java
@@ -3,8 +3,12 @@ package de.sirywell.luckpermspromoter;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.node.Node;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
@@ -27,6 +31,7 @@ public class LuckPermsPromoterPlugin extends Plugin implements Listener {
         this.luckPerms = LuckPermsProvider.get();
         loadFromFile();
         getProxy().getPluginManager().registerListener(this, this);
+        getProxy().getPluginManager().registerCommand(this, new LPPCommand());
     }
 
     private void loadFromFile() {
@@ -54,10 +59,14 @@ public class LuckPermsPromoterPlugin extends Plugin implements Listener {
 
     @EventHandler
     public void onJoin(PostLoginEvent event) {
-        var name = event.getPlayer().getName().toLowerCase(Locale.ROOT);
+        assignRole(event.getPlayer());
+    }
+
+    private void assignRole(ProxiedPlayer player) {
+        var name = player.getName().toLowerCase(Locale.ROOT);
         var role = this.playerRoles.get(name);
         if (role == null) return;
-        var user = this.luckPerms.getPlayerAdapter(ProxiedPlayer.class).getUser(event.getPlayer());
+        var user = this.luckPerms.getPlayerAdapter(ProxiedPlayer.class).getUser(player);
         var result = user.data().add(
                 Node.builder("group." + role)
                         .expiry(Duration.ofDays(2L))
@@ -66,6 +75,30 @@ public class LuckPermsPromoterPlugin extends Plugin implements Listener {
         if (result.wasSuccessful()) {
             this.luckPerms.getUserManager().saveUser(user);
             this.playerRoles.remove(name);
+        }
+    }
+
+    private final class LPPCommand extends Command {
+
+        public LPPCommand() {
+            super("lpp");
+        }
+
+        @Override
+        public void execute(CommandSender sender, String[] args) {
+            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission("luckpermspromoter.reload")) {
+                    var component = new TextComponent("You're not permitted to use that command.");
+                    component.setColor(ChatColor.RED);
+                    sender.sendMessage(component);
+                    return;
+                }
+                loadFromFile();
+                for (ProxiedPlayer player : getProxy().getPlayers()) {
+                    assignRole(player);
+                }
+                sender.sendMessage(new TextComponent("Reloaded from file."));
+            }
         }
     }
 }
